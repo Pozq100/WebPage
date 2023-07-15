b5776f4a315dc19e2200aa57ff7206a9f2c41d86
confusednut <wongziyang01@gmail.com>
2023-07-15 18:47:07 +0800

    update

--- app.py
+++ app.py
@@ -7,47 +7,47 @@ import DataGeneration
 from flask import Flask, render_template, make_response
 app = Flask(__name__)
 csvfile = "Alldatas.csv"
-line = 3
+line = 2
 @app.route('/', methods=["GET", "POST"])
 def temp_html():
     global line
-    line = 3
+    line = 2
     return render_template('test_index.html')
 
 @app.route('/index.html')
 def index_html():
     global line
-    line = 3
+    line = 2
     return render_template('index.html')
 
 @app.route('/temperature.html')
 def temperature_html():
     global line
-    line = 3
+    line = 2
     return render_template('temperature.html')
 
 @app.route('/humidity.html')
 def humidity_html():
     global line
-    line = 3
+    line = 2
     return render_template('humidity.html')
 
 @app.route('/ec.html')
 def ec_html():
     global line
-    line = 3
+    line = 2
     return render_template('ec.html')
 
 @app.route('/ph.html')
 def ph_html():
     global line
-    line = 3
+    line = 2
     return render_template('ph.html')
 
 @app.route('/light.html')
 def light_html():
     global line
-    line = 3
+    line = 2
     return render_template('light.html')
 
 @app.route('/data', methods=["GET", "POST"])
@@ -67,6 +67,7 @@ def data():
         response = make_response(json.dumps(0))
         response.content_type = 'application/json'
         return response
+
 @app.route('/data_current', methods=["GET", "POST"])
 def data2():
     global line
