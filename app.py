39c14b5b3294847154b93923ffde1010c9e5c56c
Pozq100 <chenjc.22@ichat.sp.edu.sg>
2023-07-15 17:32:19 +0800

    Updated app.py

--- app.py
+++ app.py
@@ -10,30 +10,44 @@ csvfile = "Alldatas.csv"
 line = 2
 @app.route('/', methods=["GET", "POST"])
 def temp_html():
+    global line
+    line = 2
     return render_template('test_index.html')
 
 @app.route('/index.html')
 def index_html():
+    global line
+    line = 2
     return render_template('index.html')
 
 @app.route('/temperature.html')
 def temperature_html():
+    global line
+    line = 2
     return render_template('temperature.html')
 
 @app.route('/humidity.html')
 def humidity_html():
+    global line
+    line = 2
     return render_template('humidity.html')
 
 @app.route('/ec.html')
 def ec_html():
+    global line
+    line = 2
     return render_template('ec.html')
 
 @app.route('/ph.html')
 def ph_html():
+    global line
+    line = 2
     return render_template('ph.html')
 
 @app.route('/light.html')
 def light_html():
+    global line
+    line = 2
     return render_template('light.html')
 @app.route('/data', methods=["GET", "POST"])
 def data():
