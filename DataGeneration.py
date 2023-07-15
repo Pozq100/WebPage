77c032177155b59bb9d9bcdc36ac8957823f280d
Pozq100 <chenjc.22@ichat.sp.edu.sg>
2023-07-15 17:26:31 +0800

    Updated app.py

--- DataGeneration.py
+++ DataGeneration.py
@@ -1,8 +1,36 @@
 import random as rng
+import csv
 def DataGeneration():
     Temperature = round(rng.uniform(20, 40), 1)
     Humidity = round(rng.uniform(0, 100), 1)
     EC_level = round(rng.uniform(0, 1), 0)
     pH_level = round(rng.uniform(0, 14), 0)
     light_level = round(rng.uniform(0, 1023), 0)
+    WriteData([Temperature,Humidity,EC_level,pH_level,light_level])
     return [Temperature,Humidity,EC_level,pH_level,light_level]
+
+def WriteData(Datas):
+    with open(csvfile, 'a', newline="") as Data:
+        writer = csv.writer(Data)
+        writer.writerow(Datas)
+    return
+def ReadData(index):
+    # Temperature: 1, Humidity: 2, EC_level: 3, pH_level: 4, light_level:5
+    datatype = {
+        1: "Temperature",
+        2: "Humidity",
+        3: "EC_level",
+        4: "pH_level",
+        5: "light_level"
+    }
+    temp = pd.read_csv(csvfile, usecols=[datatype[index]])
+    return temp
+def ReadLine(csvfile,line):
+    data = []
+    with open(csvfile, "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for _ in range(line - 1):
+            next(reader)
+        for i in next(reader):
+            data.append(float(i))
+    return data
--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import DataGeneration
 from flask import Flask, render_template, make_response
 app = Flask(__name__)
 csvfile = "Alldatas.csv"
+line = 2
 @app.route('/', methods=["GET", "POST"])
 def temp_html():
     return render_template('test_index.html')
@@ -34,37 +35,18 @@ def ph_html():
 @app.route('/light.html')
 def light_html():
     return render_template('light.html')
-
 @app.route('/data', methods=["GET", "POST"])
 def data():
     # Data Format
     # [TIME, Temperature, Humidity]
+    global line
+    line += 1
     current_time = (time() + 28800000) * 1000
-    WriteData(DataGeneration.DataGeneration())
-    data = [current_time] + DataGeneration.DataGeneration()
-    print(ReadData(1))
+    data = [current_time] + DataGeneration.ReadLine(csvfile, line)
     response = make_response(json.dumps(data))
 
     response.content_type = 'application/json'
 
     return response
-
-def WriteData(Datas):
-    with open(csvfile, 'a', newline="") as Data:
-        writer = csv.writer(Data)
-        writer.writerow(Datas)
-    return
-def ReadData(index):
-    # Temperature: 1, Humidity: 2, EC_level: 3, pH_level: 4, light_level:5
-    datatype = {
-        1: "Temperature",
-        2: "Humidity",
-        3: "EC_level",
-        4: "pH_level",
-        5: "light_level"
-    }
-    temp = pd.read_csv(csvfile, usecols=[datatype[index]])
-    return temp
-
 if __name__ == "__main__":
     app.run(debug=True)
