c13ac6ba681a34d360e11829a72b29bc8396d536
Pozq100 <chenjc.22@ichat.sp.edu.sg>
2023-07-17 13:29:20 +0800

    Update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 from threading import Thread
+import app
+import DataGeneration
 import light_intensity
 import ec_level
 import temperature
@@ -10,39 +12,44 @@ from hal import hal_temp_humidity_sensor as temp_humid_sensor
 def potentiometer_reading():
     Potentiometer = adc.get_adc_value(1)
     print("Potentiometer: " + str(Potentiometer))
-
+    return Potentiometer
 def temperature_humidity_reading():
     Temperature, Humidity = temp_humid_sensor.read_temp_humidity()
     print("Temperature : " + str(Temperature))
     print("Humidity : " + str(Humidity))
-
+    return [Temperature, Humidity]
 def ldr_reading():
     LDR = adc.get_adc_value(0)
     print("LDR: " + str(LDR))
-
+    return LDR
 def moisture_reading():
     Moisture_sensor = moisture_sensor.read_sensor()
     print("Moisture: " + str(Moisture_sensor))
-
+    return Moisture_sensor
 def main():
     adc.init()
     moisture_sensor.init()
     temp_humid_sensor.init()
-
+    app.run(debug=True)
     light_intensity_thread = Thread(target=light_intensity.main)
     ec_level_thread = Thread(target=ec_level.main)
     temperature_thread = Thread(target=temperature.main)
+    webpage_thread = Thread(target=DataGeneration)
+    webpage2_thread = Thread(target=app)
+    webpage2_thread.start()
     light_intensity_thread.start()
     ec_level_thread.start()
+    webpage_thread.start()
     temperature_thread.start()
 
     while True:
-        potentiometer_reading()
-        temperature_humidity_reading()
-        ldr_reading()
-        moisture_reading()
+        pH_level = potentiometer_reading()
+        Temperature, Humidity = temperature_humidity_reading()
+        light_level = ldr_reading()
+        EC_level = moisture_reading()
+        DataGeneration.DataGeneration(Temperature, Humidity, EC_level, pH_level, light_level)
         print(" ")
-        time.sleep(1)
+        time.sleep(10)
 
 if __name__ == '__main__':
     main()
